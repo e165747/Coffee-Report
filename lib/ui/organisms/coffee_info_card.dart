6b764c46fa48f69fee1590fb801363aa942698b8
Kohei Kina <e165747@ie.u-ryukyu.ac.jp>
2022-07-23 16:03:14 +0900

    コーヒー名表示用カードWidgetをListViewとListTileで表示させるよう修正

--- lib/ui/organisms/coffee_info_card.dart
+++ lib/ui/organisms/coffee_info_card.dart
@@ -1,67 +1,32 @@
 import 'package:flutter/material.dart';
 
+const defaultMessage = '--';
+
 class CoffeeInfoCard extends StatelessWidget {
-  String name;
-  String description;
-  String country;
-  CoffeeInfoCard(
-      {Key? key,
-      required this.name,
-      required this.description,
-      required this.country})
-      : super(key: key);
+  Map<String, String> cofeeInfo;
+  CoffeeInfoCard({
+    Key? key,
+    required this.cofeeInfo,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      // color: Colors.brown.withOpacity(0.1),
-      color: Colors.white.withOpacity(0.75),
-      child: Row(
-        children: [
-          Expanded(
-            flex: 1,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: SizedBox(height: 40, child: Icon(Icons.flag)),
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 7,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Column(
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      child: Text(
-                        '${name}',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 16),
-                      ),
-                    ),
-                    Container(
-                        width: double.infinity,
-                        child: Text('${description}',
-                            style: TextStyle(
-                                color: Colors.black.withOpacity(0.7)))),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Align(
-              alignment: Alignment.centerRight,
-              child: SizedBox(height: 40, child: Icon(Icons.navigate_next)),
-            ),
-          )
-        ],
+      child: ListTile(
+        leading: Icon(
+          Icons.flag,
+          size: 40.0,
+        ),
+        title: Text(
+          // '${name}',
+          '${cofeeInfo['name'] ?? defaultMessage}',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+        ),
+        subtitle: Text('${cofeeInfo['description'] ?? defaultMessage}'),
+        trailing: Icon(Icons.more_vert),
+        onTap: () {
+          print('tap');
+        },
       ),
     );
   }
--- lib/ui/pages/top.dart
+++ lib/ui/pages/top.dart
@@ -10,15 +10,6 @@ class Top extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.brown,
         scaffoldBackgroundColor: Colors.white,
       ),
@@ -29,17 +20,10 @@ class Top extends StatelessWidget {
   }
 }
 
+const defaultMessage = '--';
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -56,7 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
     },
     {
       'country': 'ethiopia',
-      'description': 'Sweet and Bitter',
+      // 'description': 'Sweet and Bitter',
       'name': 'moca',
     },
   ];
@@ -72,17 +56,13 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: <Widget>[
-              Column(
-                children: coffeeInfo
-                    .map((Map<String, String> e) => CoffeeInfoCard(
-                        name: '${e['name']}',
-                        description: '${e['description']}',
-                        country: '${e['country']}'))
-                    .toList(),
-              ),
-            ],
+          child: ListView.builder(
+            itemBuilder: (BuildContext context, int index) {
+              return CoffeeInfoCard(
+                cofeeInfo: coffeeInfo[index],
+              );
+            },
+            itemCount: coffeeInfo.length,
           ),
         ),
       ),
